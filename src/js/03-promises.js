8300df0f271950771c41d86708572d3b9c293329
Iris2030 <ibazhinova@gmail.com>
2021-11-28 15:54:35 +0200

    hw 9 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,31 +5,14 @@ const inputDelay = document.querySelector('[name = delay]')
 const step = document.querySelector('[name = step]')
 const amount = document.querySelector('[name = amount]')
 
-let   DELAY = 0
-let stepDelay = 0
-let amountNumber = 0
-let intervalId = null
+
  form.addEventListener('submit', onFormSubmit)
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-
-    delay = DELAY
-    position = 0
-
-    for (let i = 0; i < amountNumber; i += 1){
-      
-      delay += stepDelay
-      position += 1
-
-      console.log('position', position);
-      console.log('iterator', i);
-      console.log('delay', delay);
-      }
-     
-  const shouldResolve = Math.random() > 0.3;
- 
   
+  const shouldResolve = Math.random() > 0.3;
+
     setTimeout(() => {
  
   if (shouldResolve) {
@@ -38,7 +21,7 @@ resolve({position, delay})
   reject({position, delay})
   }
                 
-        }, DELAY)
+        }, delay)
   })
 }
 
@@ -48,34 +31,24 @@ resolve({position, delay})
 function onFormSubmit(event) {
   event.preventDefault()  
   
-  
-  intervalId = setInterval(() => {
+let delay = Number(inputDelay.value)
+let amountNumber = Number(amount.value)
+let stepDelay = Number(step.value)
 
-    for (let i = 0; i < amountNumber; i += 1) {
+
+    for (let i = 1; i < amountNumber; i += 1) {
  
-    createPromise()
+    createPromise(i, delay)
   .then(({ position, delay }) => {
    Notiflix.Notify.warning(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
     Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
   })
-         if (amountNumber) {
-clearInterval(intervalId)
+
+     delay += stepDelay 
 }
-     }
-    }, stepDelay)
-  
 
-
-DELAY = Number(inputDelay.value)
-amountNumber = Number(amount.value)
-stepDelay = Number(step.value)
-  
-
-  // console.log(DELAY);
-  // console.log(stepDelay);
-  // console.log(amountNumber);
 }
 
 
